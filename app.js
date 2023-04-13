7c989ad85df7f87ee41bb950f197371a67915b85
DevAntonioRogers <ar919061493@gmail.com>
2023-04-13 19:47:56 -0400

    File Uploads

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 
@@ -63,6 +64,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
+app.use('/imageUpload', uploadRouter);
 
 
 app.use(function (req, res, next) {
